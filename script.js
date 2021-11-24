c4f7cf2e423300c9eb774fb0dc91b1dffdca0907
João Caio Lopes Bernardino <jocaio@uol.com.br>
2021-11-24 10:24:26 -0300

    Ex aulab7w gerando e manipulando datas

---
+++ script.js
@@ -0,0 +1,55 @@
+let d = new Date();
+
+d.setMonth(11);
+
+let novoValor = d;
+console.log(novoValor);
+
+/*
+// Como 'setar' o ano determinado, os demais segue a mesma lógica
+let d = new Date();
+
+d.setFullYear(2022);
+
+let novoValor = d;
+*/
+
+
+
+/*
+// Date.now data de 1 de janeiro de 1970 até hj... data para banco de dados
+let d = new Date();
+
+let novoValor = Date.now();
+*/
+
+
+/*
+// getDate... pega o dia atual
+let d = new Date();
+
+let novoValor = d.getDate();
+*/
+
+
+/*
+// getDay Retorna o dia da semana de 0 a 6
+let d = new Date();
+
+let novoValor = d.getDay();
+*/
+
+/*
+// Como pega o mês, Lembrando mês começa no 0 
+let d = new Date();
+
+let novoValor = d.getMonth();
+*/
+
+
+/*
+// Como pegar somente o ano
+let d = new Date();
+
+let novoValor = d.getFullYear();
+*/
